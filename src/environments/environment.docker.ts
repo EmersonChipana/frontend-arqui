ccd585c536d43739f657b652344c470bd671a1cf
EmersonChipana <emerson.chipana@ucb.edu.bo>
2023-04-01 20:49:14 -0400

    Error keycloak

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   url: "http://localhost:4200/backend/api/v1/exchange",
-  url_keycloak:"http://localhost:8080"
+  url_keycloak:"http://172.7.0.1:8080"
 };
   
   
