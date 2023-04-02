6bc518f6bb0212af77acaa0835e4cf71a3c18870
EmersonChipana <emerson.chipana@ucb.edu.bo>
2023-04-02 17:55:23 -0400

    Docker funcionando

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   url: "http://localhost:4200/backend/api/v1/exchange",
-  url_keycloak:"http://172.7.0.1:8080"
+  url_keycloak:"http://192.168.0.9:8080"
 };
   
   
